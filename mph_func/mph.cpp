3282452ddc189391e868801d5c0ddfc33cda4f65
Ankit Modi <ankit.5036@gmail.com>
2023-08-02 09:23:48 +0530

    added sample for multiple Exception Handling

--- mph_func/mph.cpp
+++ mph_func/mph.cpp
@@ -1,14 +1,22 @@
 // Mile per Hour - Exception Handling
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 double calculate_mph(int miles, int hours)
 {
+//  Throw Zero Value Error
     if (hours == 0)
     {
         throw 0;
+    }
+
+//  Throw Negative Value Error
+    if ((miles < 0) || (hours < 0))
+    {
+        throw string{"Negative Value Error"};
     }
     
     return static_cast<double> (miles) / hours;
@@ -25,16 +33,29 @@ int main(void)
     cout << "Enter the hours:";
     cin >> hours;
     
-//    Exception Handling
+//  Exception Handling
     try
     {
         calculate_mph(miles, hours);
         cout << "Miles per Hour: " << miles_per_hour << endl;
     }
 
+// Catch Zero Value Error using Integer Argument
     catch(int &ex_err)
     {
         cerr << "Sorry, Can't divide by Zero." <<endl;
+    }
+
+//  Catch Negative Value Error using String Argument
+    catch(string &ex)
+    {
+        cerr << ex << endl;
+    }
+
+//  Catch Any other type of exception handling
+    catch(...)
+    {
+        cerr << "Unknown Error" << endl;
     }
     
     cout << "Bye!!!" << endl;
