82543b1dfdee8ed767dcef6cca008f2bc31edd72
Ankit Modi <ankit.5036@gmail.com>
2023-08-21 09:33:48 +0530

    Added File Manipulation - Read File as character

---
+++ read_file4/main.cpp
@@ -0,0 +1,23 @@
+// Read File 4
+// Read and display Shakespeare Sonnet 18 poem using unformatted get
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream in_file;
+    in_file.open("poem.txt");
+    if (!in_file) {
+        cerr << "Problem opening file" << endl;
+        return 1;
+    }
+
+    char c {};
+    while (in_file.get(c)) {
+        cout << c;
+    }
+    cout << endl;
+    in_file.close();
+    return 0;
+}
